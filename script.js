8903c415a2e2cf04cecdc2fb329d7e2c3f84146c
@amritmohan <amritmohan2284@gmail.com>
2025-02-23 17:55:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+let squares = document.querSelectorAll(".square");
+
+function onEnter(event) {
+	squares.foreach(square => {
+		if (square !== event.target) {
+			square.style.backgroundColor = "#6F4E37"; 
+		}
+	});
+}
+
+function onLeave() {
+	squares.forEach(square =>{
+		square.style.backgroundColor = "#E6E6FA";
+	});
+}
+
+squares.forEach(square =>{
+	square.addEventListener("Oncursor", onEnter);
+	square.addEvenetListener("Offcursor", onLeave);
+});
